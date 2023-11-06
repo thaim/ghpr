8194a4b78a447d94c6a12761b8a18a4b935082db
thaim <thaim24@gmail.com>
2023-11-06 22:45:42 +0900

    add options

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,16 @@ const program = new Command();
 program
     .name('ghpr')
     .version('0.1.0')
+    .option('-u, --user <username>')
+    .option('-r, --repo <repository name>');
 
-program.parse();
+program.parse(process.argv);
+const options = program.opts();
 
-const main = () => {
-    console.log("hello");
+const main = async (user: string, repo: string) => {
+    const octokit = new Octokit();
+
+    console.log(`Hello from main --user ${user} --repo ${repo}`);
 }
 
-main()
+main(options.user, options.repo);
