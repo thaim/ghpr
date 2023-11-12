da418b28067c7c5ee615fa875169f40841f45453
thaim <thaim24@gmail.com>
2023-11-12 23:37:40 +0900

    implement regexp option to filter by repository name

--- src/main.ts
+++ src/main.ts
@@ -8,16 +8,38 @@ program
     .name('ghpr')
     .version('0.1.0')
     .option('-u, --user <username>')
-    .option('-r, --repo <repository name>');
+    .option('-r, --repo <repository name>')
+    .option('--repo-regexp <repository regexp>');
 
 program.parse(process.argv);
 const options = program.opts();
 
-const main = async (user: string, repo: string) => {
+const main = async (user: string, repos: string, repoRegexp: string) => {
     const pat = process.env.PAT;
     const octokit = new Octokit({
         auth: pat
     });
+
+    if (repos === undefined && repoRegexp != "") {
+        type listReposiotryParameters = Endpoints["GET /users/{username}/repos"]["parameters"];
+        type listReposiotryResponse = Endpoints["GET /users/{username}/repos"]["response"];
+        const params: listReposiotryParameters = {
+            username: user,
+            type: "all",
+        };
+        const { data } = await octokit.request(
+            "GET /users/{username}/repos",
+            params
+        ) as listReposiotryResponse;
+        const regexp = new RegExp(repoRegexp);
+
+        repos = data
+            .filter((repo: any) => repo.name.match(regexp))
+            .map((repo: any) => repo.name)
+            .join(',');
+
+        console.log(`repos: ${repos}`);
+    }
 
     repos.split(',').forEach(async (repo: string) => {
         await describeRepository(octokit, user, repo);
@@ -45,4 +67,4 @@ const describeRepository = async (octokit: any, user: string, repo: string) => {
     });
 }
 
-main(options.user, options.repo);
+main(options.user, options.repo, options.repoRegexp);
